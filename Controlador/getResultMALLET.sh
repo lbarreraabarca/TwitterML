8f47da06cbef17a0f1d06f00aa57d252704becca
lbarreraabarca <lu.barreraabarca@gmail.com>
2019-01-14 16:42:42 +0100

    Script que obtiene los resultados

---
+++ Controlador/getResultMALLET.sh
@@ -0,0 +1,21 @@
+now="$(date)"
+echo "[ getFiles ][ getFiles.sh Inicio ][ "$now" ]"
+
+cd ../MineriaDeDatos/Mallet/Twitter/Data
+
+rm ../../../../Controlador/resultados.dat
+
+for i in */vectorInicial/log/*
+do
+  accu=$(cat $i | grep "accuracy mean =")
+  f1=$(cat $i | grep "f1")
+  file=$(echo $i | awk '{split($0, data, "log"); print substr(data[2],2, length(data[2])-2)}')
+  echo $file    >> ../../../../Controlador/resultados.dat
+  echo $f1      >> ../../../../Controlador/resultados.dat
+  echo $accu    >> ../../../../Controlador/resultados.dat
+  echo "--------------------"   >> ../../../../Controlador/resultados.dat
+done
+
+now="$(date)"
+echo "[ getFiles ][ getFiles.sh Terminado ][ "$now" ]"
+cd ../../../../Controlador
