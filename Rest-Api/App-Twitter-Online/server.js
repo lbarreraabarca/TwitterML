771218fdd91a44ee8126d09e00d77e2c4d807e69
Luis Barrera <luis.barrera@schibsted.com>
2019-09-24 15:27:59 -0300

    Set URL Server

--- Rest-Api/App-Twitter-Online/server.js
+++ Rest-Api/App-Twitter-Online/server.js
@@ -5,7 +5,7 @@ var http = require('http');
 var server;
 
 server = http.createServer(app);
-server.listen(process.env.PORT || 80);
+server.listen(process.env.PORT || 8080);
 server.on('listening', function () {
     console.log('Server listening on http://localhost:%d', this.address().port);
 });
--- Rest-Api/App-Twitter-Online/view/monTwitterOnline/monTwitterHome/js/cargaUrlServer.js
+++ Rest-Api/App-Twitter-Online/view/monTwitterOnline/monTwitterHome/js/cargaUrlServer.js
@@ -1,2 +1,2 @@
 //var urlServer="http://40a1cac3.ngrok.io/";
-var urlServer="http://http://18.189.180.131:80/";
+var urlServer="http://http://18.189.180.131:8080/";
