ba91f073cb8389e48e700a3e1ba6bb385160d812
Luis Barrera <luis.barrera@schibsted.com>
2019-09-24 16:56:18 -0300

    Test

--- Stream/DN-Twitter/controller/main.sh
+++ Stream/DN-Twitter/controller/main.sh
@@ -7,7 +7,7 @@ echo "cd $PCONTROL"
 rm output* working_data/*
 
 # NUMERO DE INSTANCIAS
-N=5
+N=3
 
 # obtener listado de proxies
 JSOUP=/home/centos/dev/git/TwitterML/Stream/DN-Twitter/getProxiesJSOUP
@@ -36,25 +36,9 @@ make
 make PROXYFILE=$PCONTROL/proxies.dat CTAFILE=$PCONTROL/cuenta_test.dat run
 mv $CK_PROX/ok_proxies.dat $PCONTROL
 
-# particionar los proxies correctos
-cd $PCONTROL
-echo "cd $PCONTROL"
 
+PARTITION=/home/centos/dev/git/TwitterML/Stream/DN-Twitter/script
+cd $PARTITION
+echo "cd $PARTITION"
 
-# comenzar la descarga
-cd /home/lbarrera/PROYECTOS/PRACTICA-201701-carmona-cifuentes/DN-Twitter/twitter_proxy
-echo "cd /home/lbarrera/PROYECTOS/PRACTICA-201701-carmona-cifuentes/DN-Twitter/twitter_proxy"
-
-make clean
-make
-
-FILETERMS=$PCONTROL/terminos.dat
-FILECUENTAS=$PCONTROL/cuentas.dat
-for n in $(seq 0 $(echo "$N-1" | bc -l)); do
-  echo "INSTANCIA=$n"
-
-  FILEOUT=$PCONTROL/working_data/data$n.dat
-  FILEPROXY=$PCONTROL/proxy$n.dat
-  FILE=$PCONTROL/output$n.out
-  sh run.sh $n $n $FILEOUT $FILEPROXY $FILETERMS $FILECUENTAS > $FILE &
-done
+bash particiona.sh $PCONTROL/ok_proxies.dat $N
